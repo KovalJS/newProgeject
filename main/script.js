690d64e469777d6edbe8f09dbb82803759bfa0ba
Koval Alexander <x-sania@mail.ru>
2019-11-08 22:08:36 +0500

    Урок 11.2 основное

--- main/script.js
+++
@@ -1,9 +0,0 @@
-let money = 'деньги',
-    income = 'доход',
-    addExpenses = 'затраты',
-    deposit = 'депозит',
-    mission = 'миссия',
-    period = 'период';
-
-alert('Hello World');
-console.log('Hello frends');
